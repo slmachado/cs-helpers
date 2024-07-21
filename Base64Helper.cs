1f6fc4917a6b09917f05d011a3b12b074d90ab46
Sergio Machado <sergio-software-engineer@hotmail.com>
2024-07-21 22:05:26 +0100

    Summary and documentation Base64Helper.cs e BinaryHelper.cs

--- Base64Helper.cs
+++ Base64Helper.cs
@@ -1,13 +1,23 @@
 ﻿namespace Helpers;
 
+/// <summary>
+/// Provides methods for working with Base64 encoding and decoding.
+/// </summary>
 public static class Base64Helper
 {
     /// <summary>
-    /// Decode base64 string
-    /// Check base64 length and add characters to reach base64 length
+    /// Safely decodes a Base64-encoded string into a byte array.
+    /// <para>
+    /// The method replaces URL-safe characters ('-' and '_') with the standard Base64 characters ('+' and '/').
+    /// It then ensures that the string length is a multiple of 4 by appending '=' padding characters as necessary.
+    /// </para>
+    /// <para>
+    /// If the input string is null, empty, or consists only of white spaces, the method returns an empty byte array.
+    /// In case of any errors during the decoding process (e.g., if the input is not valid Base64), the method also returns an empty byte array.
+    /// </para>
     /// </summary>
-    /// <param name="input"></param>
-    /// <returns></returns>
+    /// <param name="input">The Base64-encoded string to decode.</param>
+    /// <returns>A byte array representing the decoded data, or an empty byte array if the input is invalid or an error occurs.</returns>
     public static byte[] SafeConvertFromBase64String(string input)
     {
         if (string.IsNullOrWhiteSpace(input)) return Array.Empty<byte>();
--- BinaryHelper.cs
+++ BinaryHelper.cs
@@ -1,125 +1,154 @@
-namespace Helpers;
-
-/// <summary>
-/// helper to get binary value from Word
-/// </summary>
-public static class BinaryHelper
+namespace Helpers
 {
-    #region Convert to binary string
     /// <summary>
-    /// Get binary string on 64 bits from long
+    /// Helper to get binary value from Word
     /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    public static string ToBinary(this long value)
+    public static class BinaryHelper
     {
-        return Convert.ToString(value, 2).PadLeft(64, '0');
-    }
-    #endregion
+        #region Convert to binary string
 
-    #region Get Value from binary string
-    public static bool GetBoolFromBinaryWord(string binaryWord, int position)
-    {
-        if (String.IsNullOrEmpty(binaryWord))
+        /// <summary>
+        /// Get binary string on 64 bits from long
+        /// </summary>
+        /// <param name="value">The long value to convert to a binary string.</param>
+        /// <returns>A 64-bit binary string representation of the specified long value.</returns>
+        public static string ToBinary(this long value)
         {
-            throw new ArgumentNullException(nameof(binaryWord));
+            return Convert.ToString(value, 2).PadLeft(64, '0');
         }
 
-        if (position < 0 || position >= binaryWord.Length)
+        #endregion
+
+        #region Get Value from binary string
+
+        /// <summary>
+        /// Get bool value from a specific position in a binary string.
+        /// </summary>
+        /// <param name="binaryWord">The binary string to evaluate.</param>
+        /// <param name="position">The position of the bit to evaluate.</param>
+        /// <returns>True if the bit at the specified position is 1, otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when binaryWord is null or empty.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when position is out of range.</exception>
+        public static bool GetBoolFromBinaryWord(string binaryWord, int position)
         {
-            throw new ArgumentOutOfRangeException(nameof(position));
-        }
-
-        int startIndex = (binaryWord.Length-1) - position;
-        string binaryValue = binaryWord.Substring(startIndex, 1);
-
-        return binaryValue.Equals("1");
-    }
-
-    public static string GetBinaryValueFromBinaryWord(string binaryWord, int startBitPosition, int endBitPosition)
-    {
-        if (String.IsNullOrEmpty(binaryWord))
-        {
-            throw new ArgumentNullException(nameof(binaryWord));
-        }
-
-        if (startBitPosition < 0 || startBitPosition >= binaryWord.Length)
-        {
-            throw new ArgumentOutOfRangeException(nameof(startBitPosition));
-        }
-
-        if (endBitPosition < 0 || endBitPosition > binaryWord.Length)
-        {
-            throw new ArgumentOutOfRangeException(nameof(endBitPosition));
-        }
-
-        if (startBitPosition >= endBitPosition)
-        {
-            throw new ArgumentException("EndBitPosition must be superior to StartBitPosition");
-        }
-
-        int startIndex = (binaryWord.Length) - endBitPosition;
-        int length = endBitPosition - startBitPosition;
-        return binaryWord.Substring(startIndex, length);
-    }
-    #endregion
-
-    #region Get Value from word
-
-    /// <summary>
-    /// Get bool from word
-    /// </summary>
-    /// <param name="value">word</param>
-    /// <param name="position">Bit position</param>
-    /// <returns></returns>
-    public static bool GetBoolFromWord(this long value, int position)
-    {
-        string binaryWord = value.ToBinary();
-        return GetBoolFromBinaryWord(binaryWord, position);
-    }
-
-
-    /// <summary>
-    /// Get binary value from binary word
-    /// </summary>
-    /// <param name="value"></param>
-    /// <param name="startBitPosition"></param>
-    /// <param name="endBitPosition"></param>
-    /// <returns></returns>
-    public static string GetBinaryValueFromBinaryWord(this long value, int startBitPosition, int endBitPosition)
-    {
-        string binaryWord = value.ToBinary();
-        return GetBinaryValueFromBinaryWord(binaryWord, startBitPosition, endBitPosition);
-    }
-
-    public static int? GetValueFromWordBinary(this double valueDouble, int startBitPosition, int endBitPosition,  bool isInverted = false)
-    {
-        long value = Convert.ToInt64(valueDouble);
-        int? result = null;
-        if (startBitPosition == endBitPosition || (startBitPosition + 1) == endBitPosition)
-        {
-            // Boolean
-            bool bValue = value.GetBoolFromWord(startBitPosition); 
-            if (isInverted)
+            if (String.IsNullOrEmpty(binaryWord))
             {
-                bValue = !bValue;
+                throw new ArgumentNullException(nameof(binaryWord));
             }
-            result = bValue ? 1 : 0;
-        }
-        else
-        {
-            // Enum
-            string strValue = value.GetBinaryValueFromBinaryWord(startBitPosition,
-                endBitPosition);
-            if (String.IsNullOrEmpty(strValue) == false)
+
+            if (position < 0 || position >= binaryWord.Length)
             {
-                result = Convert.ToInt32(strValue, 2);
+                throw new ArgumentOutOfRangeException(nameof(position));
             }
+
+            int startIndex = (binaryWord.Length - 1) - position;
+            string binaryValue = binaryWord.Substring(startIndex, 1);
+
+            return binaryValue.Equals("1");
         }
 
-        return result;
+        /// <summary>
+        /// Get a substring of the binary string between the specified start and end bit positions.
+        /// </summary>
+        /// <param name="binaryWord">The binary string to evaluate.</param>
+        /// <param name="startBitPosition">The start position of the bit range.</param>
+        /// <param name="endBitPosition">The end position of the bit range.</param>
+        /// <returns>A binary substring from the specified range.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when binaryWord is null or empty.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when startBitPosition or endBitPosition are out of range.</exception>
+        /// <exception cref="ArgumentException">Thrown when endBitPosition is not greater than startBitPosition.</exception>
+        public static string GetBinaryValueFromBinaryWord(string binaryWord, int startBitPosition, int endBitPosition)
+        {
+            if (String.IsNullOrEmpty(binaryWord))
+            {
+                throw new ArgumentNullException(nameof(binaryWord));
+            }
+
+            if (startBitPosition < 0 || startBitPosition >= binaryWord.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startBitPosition));
+            }
+
+            if (endBitPosition < 0 || endBitPosition > binaryWord.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endBitPosition));
+            }
+
+            if (startBitPosition >= endBitPosition)
+            {
+                throw new ArgumentException("EndBitPosition must be superior to StartBitPosition");
+            }
+
+            int startIndex = (binaryWord.Length) - endBitPosition;
+            int length = endBitPosition - startBitPosition;
+            return binaryWord.Substring(startIndex, length);
+        }
+
+        #endregion
+
+        #region Get Value from word
+
+        /// <summary>
+        /// Get bool value from a specific bit position in a long word.
+        /// </summary>
+        /// <param name="value">The long value to evaluate.</param>
+        /// <param name="position">The position of the bit to evaluate.</param>
+        /// <returns>True if the bit at the specified position is 1, otherwise false.</returns>
+        public static bool GetBoolFromWord(this long value, int position)
+        {
+            string binaryWord = value.ToBinary();
+            return GetBoolFromBinaryWord(binaryWord, position);
+        }
+
+        /// <summary>
+        /// Get a binary substring from a specific bit range in a long word.
+        /// </summary>
+        /// <param name="value">The long value to evaluate.</param>
+        /// <param name="startBitPosition">The start position of the bit range.</param>
+        /// <param name="endBitPosition">The end position of the bit range.</param>
+        /// <returns>A binary substring from the specified range.</returns>
+        public static string GetBinaryValueFromBinaryWord(this long value, int startBitPosition, int endBitPosition)
+        {
+            string binaryWord = value.ToBinary();
+            return GetBinaryValueFromBinaryWord(binaryWord, startBitPosition, endBitPosition);
+        }
+
+        /// <summary>
+        /// Get integer value from a specific bit range in a double word, with an option to invert the boolean value.
+        /// </summary>
+        /// <param name="valueDouble">The double value to evaluate.</param>
+        /// <param name="startBitPosition">The start position of the bit range.</param>
+        /// <param name="endBitPosition">The end position of the bit range.</param>
+        /// <param name="isInverted">Whether to invert the boolean value.</param>
+        /// <returns>An integer value from the specified range, or null if the range is invalid.</returns>
+        public static int? GetValueFromWordBinary(this double valueDouble, int startBitPosition, int endBitPosition, bool isInverted = false)
+        {
+            long value = Convert.ToInt64(valueDouble);
+            int? result = null;
+            if (startBitPosition == endBitPosition || (startBitPosition + 1) == endBitPosition)
+            {
+                // Boolean
+                bool bValue = value.GetBoolFromWord(startBitPosition);
+                if (isInverted)
+                {
+                    bValue = !bValue;
+                }
+                result = bValue ? 1 : 0;
+            }
+            else
+            {
+                // Enum
+                string strValue = value.GetBinaryValueFromBinaryWord(startBitPosition,
+                    endBitPosition);
+                if (String.IsNullOrEmpty(strValue) == false)
+                {
+                    result = Convert.ToInt32(strValue, 2);
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
     }
-
-
-    #endregion
 }
